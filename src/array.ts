aad8cda054a82edc66248c5c07a3db891cd949ef
arifmia1129 <arifmia1129@gmail.com>
2023-07-14 09:24:23 +0600

    array in typescript

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+const family: string[] = ['Arif', 'Binu', 'Ariba', 'Abdullah'];
+
+const familyMembersAge: number[] = [25, 23, 2, 1];
+
+// only use those method that only related with this type
+
+// family.map(member => member.)
+// familyMembersAge.map(age => age.)
+
+// touple
+const student: [string, number, string] = ['Ariba', 1, 'female'];
+
+student[0] = 'Ariba Binte Arif';
