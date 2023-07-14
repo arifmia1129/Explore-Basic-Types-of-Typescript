8dae9fd6427031f548e5bc4f0a8df86a1fa90b7f
arifmia1129 <arifmia1129@gmail.com>
2023-07-14 09:51:04 +0600

    object

---
+++ src/object.ts
@@ -0,0 +1,15 @@
+const user: {
+  company: 'Programming Hero';
+  readonly name: string;
+  age: number;
+  isMarried: boolean;
+  spouseName?: string;
+} = {
+  company: 'Programming Hero',
+  name: 'Arif',
+  age: 23,
+  isMarried: true,
+  spouseName: 'Binu',
+};
+
+// user.name = 'Md Arif';
