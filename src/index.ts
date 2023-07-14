49b24807a97be6b80492d5ea9b760d98895514fc
arifmia1129 <arifmia1129@gmail.com>
2023-07-14 09:00:39 +0600

    primitive data types

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Primitive data types in Typescript
+
+/**
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ */
+
+// string
+const studentName: string = 'Ariba';
+
+// number
+const age: number = 10;
+
+// boolean
+const isAdult = false;
+
+// any
+let result;
